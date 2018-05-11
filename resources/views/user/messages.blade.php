18fdc937d7f9453755a03d6382d048380d3fce19
alsoghayer-astc <alsoghayer@astc.com.sa>
2018-05-12 02:26:33 +0300

    s

--- resources/views/user/messages.blade.php
+++ resources/views/user/messages.blade.php
@@ -9,7 +9,7 @@
     <div class="nm">
         <div class="text-center">
             <h2>{{$user->name}}</h2>
-            @if(!is_null(Auth::user()) && $user->id == Auth::user()->id)<a href="{{url('/user/edit')}}" class="btn btn-info text-center bttoun">تعديل</a>@endif
+            {{-- @if(!is_null(Auth::user()) && $user->id == Auth::user()->id)<a href="{{url('/user/edit')}}" class="btn btn-info text-center bttoun">تعديل</a>@endif --}}
         </div>
     </div>
 </div>
